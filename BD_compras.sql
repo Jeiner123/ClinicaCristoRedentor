2f9c01c8d44a14f89865f3e994e667079069fd34
Pierina <pierinacarranza@yahoo.com>
2016-08-03 21:29:45 -0500

    orden de compra mantenedor

--- BD_compras.sql
+++ BD_compras.sql
@@ -18,3 +18,30 @@ create table PROVEEDOR(
 	estado char(01) not null,
 	primary key(proveedorID)
 );
+
+create table TIPO_TRANSACCION(
+	codigo char(04) not null,
+	descripcion varchar(150) not null,
+	tipo char(01) not null,
+	estado char(01) not null,
+	primary key(codigo)
+)
+
+CREATE TABLE PRODUCTO(
+	codigo int not null,
+	descripcion varchar(150) not null,
+	estado char(01) not null,
+	fecha date not null,
+	primary key(codigo)
+)
+
+insert into TIPO_TRANSACCION values
+	('C001','COMPRA DE MERCADERÍAS(PRODUCTOS)','C','A'),
+	('C002','IMPORTACIÓN DE MERCADERÍAS(PRODUCTOS)','C','A'),
+	('C003','COMPRA DE MATERIA PRIMA','C','A'),
+	('C004','IMPORTACIÓN DE MATERIA PRIMA','C','A'),
+	('C005','COMPRA DE MATERIALES AUXILIARES','C','A'),
+	('C006','COMPRA DE SUMINISTROS','C','A'),
+	('C007','COMPRA DE ENVASES','C','A'),
+	('C008','COMPRA DE EMBALAJES','C','A');
+	
--- modulos/compras/bd/bd_operaciones.php
+++ modulos/compras/bd/bd_operaciones.php
@@ -38,19 +38,44 @@
 	
 		$res = mysqli_query($con,$consulta) or die (mysqli_error($con));
 			while($row = mysqli_fetch_row($res)){
-			 if($row[1]==1){
-			 	$entidad='PERSONA NATURAL';
-			 }
-			 if($row[1]==2){
-			 	$entidad='PERSONA JURÍDICA';
-			 }
+				if (utf8_decode($row[14]) == 'A'){
+					$color= "text-green";
+			        $class= "fa fa-circle";
+			        $title = "Activo";
+			        $estado = $row[14];
+			    }
+			    else{
+			        $color="text-red";
+			        $class= "fa fa-circle";
+			        $title = "Inactivo";
+			        $estado = $row[14];
+			    }
 				echo "<tr>					
-						<td>".$entidad."</td>
 						<td>".$row[0]."</td>
 						<td>".$row[3]."</td>		
 						<td>".$row[8]." ".$row[9]." ".$row[10]."</td>
-						<td>".$row[11]."</td>
-						<td>".$row[5]."</td>
+						<td style='text-align:center'>".$row[11]."</td>
+						<td style='text-align:center;'>
+								<label hidden>".$estado."</label>
+								<div class='action-buttons'>
+									<a href='javascrip:;' class='".$color."'>
+			                <i class='".$class."' title='".$title."'></i>
+				            </a>
+				           </div>
+				        </td>
+						<td style='text-align:center;'>
+							<div class='action-buttons'>
+								<a href='javascrip:;' class='text-blue' onclick='verProveedor(".$row[0].");' style='margin-right:7px;'>
+			              		<i class='fa fa-search' title='Ver'></i>
+			          		</a>
+				            <a href='javascrip:;' class='text-yellow' onclick='modificarProveedor(".$row[0].");' style='margin-right:7px;'>
+				                <i class='fa fa-pencil' title='Modificar'></i>
+				            </a>			            
+				            <a href='javascrip:;' class='text-red' onclick='eliminarProveedor(".$row[0].");' style='margin-right:7px;'>
+				                <i class='fa fa-trash' title='Eliminar'></i>
+				            </a>
+				          </div>
+							</td>
 					</tr>";
 		}
 			
@@ -95,8 +120,102 @@
 		}
 		exit();
 	}
-	
 
+	if($opc=='CC_05'){
+		$documento = $_POST['documento'];
+		$consulta = "UPDATE proveedor set estado='I' where proveedorID=$documento";
+		$res = mysqli_query($con,$consulta)or  die (mysqli_error($con));
+		if($res){
+			echo "Proveedor actualizado";
+		}else{
+			echo "Error con la actualización del proveedor";
+		}
+		exit();
+	}
+
+	if($opc=='CC_06'){
+		$consulta = "SELECT proveedorID,IF(tipoEntidad=1,concat(nombres,' ',apellidoPat,' ',apellidoMat),razonSocial),telefono FROM `proveedor` where estado='A'";
+		$res = mysqli_query($con,$consulta) or die (mysqli_error($con));
+		while($row = mysqli_fetch_row($res)){	
+			
+			echo "<tr>
+					<td >".$row[0]."</td>
+					<td>".$row[1]."</td>
+					<td style='text-align:center;'>".$row[2]."</td>
+					<td style='text-align:center;'>
+						<div class='action-buttons'>
+							<a href='javascrip:;' class='text-blue' onclick='seleccionarProveedor(\"".$row[0]."\",\"".$row[0]." - ".$row[1]."\");' style='margin-right:7px;'>
+				              <u>Seleccionar</u>
+				          </a>	            
+			          </div>
+					</td>
+				</tr>";
+		}
+		exit();
+	}
+
+	if($opc=='CC_07'){
+		$consulta = "SELECT codigo,descripcion from TIPO_TRANSACCION where tipo='C'";
+		$res = mysqli_query($con,$consulta) or die (mysqli_error($con));
+		while($row = mysqli_fetch_row($res)){	
+			
+			echo "<tr>
+					<td >".$row[0]."</td>
+					<td>".$row[1]."</td>
+					<td style='text-align:center;'>
+						<div class='action-buttons'>
+							<a href='javascrip:;' class='text-blue' onclick='seleccionarTCompra(\"".$row[0]."\",\"".$row[0]." - ".$row[1]."\");' style='margin-right:7px;'>
+				              <u>Seleccionar</u>
+				          </a>	            
+			          </div>
+					</td>
+				</tr>";
+		}
+		exit();
+	}
+	
+	if($opc=='CC_08'){
+		$consulta = "SELECT comprobanteID,descripcion FROM `comprobante_pago` WHERE compras=1";
+		$res = mysqli_query($con,$consulta) or die (mysqli_error($con));
+		while($row = mysqli_fetch_row($res)){	
+			
+			echo "<tr>
+					<td >".$row[0]."</td>
+					<td>".$row[1]."</td>
+					<td style='text-align:center;'>
+						<div class='action-buttons'>
+							<a href='javascrip:;' class='text-blue' onclick='seleccionarTComprobante(\"".$row[0]."\",\"".$row[0]." - ".$row[1]."\");' style='margin-right:7px;'>
+				              <u>Seleccionar</u>
+				          </a>	            
+			          </div>
+					</td>
+				</tr>";
+		}
+		exit();
+	}
+
+	if($opc=='CC_09'){
+		$consulta = "SELECT * FROM `PRODUCTO` WHERE estado='A'";
+		$res = mysqli_query($con,$consulta) or die (mysqli_error($con));
+		while($row = mysqli_fetch_row($res)){	
+			
+			echo "<tr>
+					<td >".$row[0]."</td>
+					<td>".$row[1]."</td>
+					<td style='text-align:center;'>
+						<div class='action-buttons'>
+							<a href='javascrip:;' class='text-blue' onclick='seleccionarProducto(\"".$row[0]."\",\"".$row[1]."\");' style='margin-right:7px;'>
+				              <u>Seleccionar</u>
+				          </a>	
+				          <a href='javascrip:;' class='text-blue' onclick='eliminarProducto(\"".$row[0]."\",\"".$row[1]."\");' style='margin-right:7px;'>
+				              <u>Eliminar</u>
+				          </a>	            
+			          </div>
+					</td>
+				</tr>";
+		}
+		exit();
+	}
 ?>
 
 
